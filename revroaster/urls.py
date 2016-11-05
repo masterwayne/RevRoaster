8717e10c637ff35efc9dc31ebb3998e54454b43f
Shubham <shubham.garg2895@gmail.com>
2016-11-05 05:30:42 -0700

    bot also beibg made

--- revroaster/urls.py
+++ revroaster/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
+from .views import BotView
 
 urlpatterns= [
 	url(r'^$',views.home,name='rev'),
 	url(r'^searchurl/$', views.home, name='url'),
-
+    url(r'^ca1b63d49f4e2f1547d15af7c3d8294b6d3d1ae4006916c62c/$',BotView.as_view())
 ]
--- revroaster/views.py
+++ revroaster/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import generic
+from django.http.response import HttpResponse
 # Create your views here.
 from .forms import URLForm
 import sentiment_mod as s
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json, requests, random, re
+from pprint import pprint
 
 def home(request):
     # if this is a POST request we need to process the form data
@@ -16,3 +22,35 @@ def home(request):
 		form = URLForm()
 
 	return render(request, 'revroaster/searchpage.html', {'form': form})
+
+class BotView(generic.View):
+    def get(self, request, *args, **kwargs):
+        if self.request.GET['hub.verify_token'] == '2318934571':
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+
+    # Post function to handle Facebook messages
+    def post(self, request, *args, **kwargs):
+        # Converts the text payload into a python dictionary
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        # Facebook recommends going through every entry since they might send
+        # multiple messages in a single call during high load
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
+                # Check to make sure the received call is a message call
+                # This might be delivery, optin, postback for other events 
+                if 'message' in message:
+                    # Print the message to the terminal
+                    pprint(message) 
+                    post_facebook_message(message['sender']['id'], message['message']['text'])    
+        return HttpResponse()
+
+def post_facebook_message(fbid, recevied_message):           
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAFsq1Lzxt4BADZBrGfZBd9LyEeX7gtb2aiKFpOluZAaMsDN2FTSEflg5B1frlmBuZC8xR5BOnngekPynQ2ZBC9mF1qMYOJdc5IN0NWJsESqZAXZBGRrniSFEfc3x2gonc23K6ZA8lMldhhlMfLA7QxvGn5Jb7PRrrZC5ZB1I48pZBjsxuRlUTEA326' 
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    pprint(status.json())
